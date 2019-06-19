a7c4990af2e0935b45bdfb46b7649dcb04c6ff5f
Tanya <taniaelmus@gmail.com>
2019-06-20 00:36:26 +0300

    kekstagram_module3-task1

--- js/main.js
+++ js/main.js
@@ -1,43 +1,60 @@
 'use strict';
+
+var pictureWrapper = document.querySelector('.pictures');
 var PHOTO_COMMENTS = ['Всё отлично!',
-'В целом всё неплохо. Но не всё.',
-'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!']
-var NAMES = ['Вася','Петя','Алина','Таня'];
-var images = [];
-var i = 0;
-for (i = 0; i < 25; i++) {
-images[i] = {
-  url : 'photos/' + (i + 1) +'.jpg',
-  likes : Math.floor(Math.random()*185)+16,
-  comments  : {
-    avatar: 'img/avatar-' + Math.floor(Math.random()*7) + '.svg',
-    message: PHOTO_COMMENTS[Math.floor(Math.random()*7)],
-    name: NAMES[Math.floor(Math.random()*4)]
-  }
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+var NAMES = ['Вася', 'Петя', 'Алина', 'Таня'];
+var NUMBER_OF_PICTURE = 25;
 
+var generateNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 };
-console.log(images[i])
-}
 
-var pictureTemplate = document.querySelector('#picture')
-    .content
-    .querySelector('.picture');
+var getData = function (images) {
+    var images = [];
 
-var renderPicture = function (picture) {
-  var pictureElement = pictureTemplate.cloneNode(true);
+    for (var i = 0; i < NUMBER_OF_PICTURE; i++) {
+      images[i] = {
+        url: 'photos/' + (i + 1) + '.jpg',
+        likes: generateNumber (max, min),
+        comments: []
+      };
 
-  pictureElement.querySelector('img').src = textContent =images.url;
-  pictureElement.querySelector('picture__likes').textContent = images.likes;
-  pictureElement.querySelector('picture__comments').textContent = images.comments;
-  return pictureElement;
-}
+      for (var j = 0; j < generateNumber(0,2); j++) {
+        images[i].comments.push({
+          avatar: 'img/avatar-' + generateNumber(0, 7) + '.svg',
+          message: PHOTO_COMMENTS[generateNumber(0, 7)],
+          name: NAMES[generateNumber(0,4)]
+        });
+        return images;
+      };
+    }
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < images.length; i++) {
-  fragment.appendChild(renderPicture(images[i]));
-}
+    var pictureTemplate = document.querySelector('#picture')
+      .content
+      .querySelector('.picture');
 
-pictureElement.appendChild(fragment);
+    var renderPicture = function (picture) {
+      var pictureElement = pictureTemplate.cloneNode(true);
+
+      pictureElement.querySelector('img').src = picture.url;
+      pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+      pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
+      return pictureElement;
+    };
+
+    function createFragment(images) {
+      var fragment = document.createDocumentFragment();
+      for (var i = 0; i < images.length; i++) {
+        fragment.appendChild(renderPicture(images[i]));
+      }
+
+      pictureWrapper.appendChild(fragment);
+
+    };
+    createFragment(images);
